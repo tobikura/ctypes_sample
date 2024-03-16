91e1923e79f64d498a9e20039f9c53d27c106542
tobikura <88867963+tobikura@users.noreply.github.com>
2024-03-16 13:13:19 +0900

    update workflow for cmake #1

--- include/sample.h
+++ include/sample.h
@@ -1,6 +1,12 @@
 #ifndef __SAMPLE_H__
 #define __SAMPLE_H__
 
+#ifdef WIN32
+#define DllExport __declspec(dllexport)
+#else
+#define DllExport
+#endif
+
 struct sample_struct
 {
     int a;
@@ -9,17 +15,17 @@ struct sample_struct
     float d;
 };
 
-int add_scaler(int a, int b);
-void copy_int(int *dst, int *src, int len);
-void add_int(int *dst, int *a, int *b, int len);
-void mul_int(int *dst, int *a, int *b, int len);
-int sum_int(int *a, int len);
-void copy_float(float *dst, float *src, int len);
-void add_float(float *dst, float *a, float *b, int len);
-void mul_float(float *dst, float *a, float *b, int len);
-float sum_float(float *a, int len);
+DllExport int add_scaler(int a, int b);
+DllExport void copy_int(int *dst, int *src, int len);
+DllExport void add_int(int *dst, int *a, int *b, int len);
+DllExport void mul_int(int *dst, int *a, int *b, int len);
+DllExport int sum_int(int *a, int len);
+DllExport void copy_float(float *dst, float *src, int len);
+DllExport void add_float(float *dst, float *a, float *b, int len);
+DllExport void mul_float(float *dst, float *a, float *b, int len);
+DllExport float sum_float(float *a, int len);
 
-void init_struct(struct sample_struct *s);
-void print_struct(struct sample_struct *s);
+DllExport void init_struct(struct sample_struct *s);
+DllExport void print_struct(struct sample_struct *s);
 
 #endif
